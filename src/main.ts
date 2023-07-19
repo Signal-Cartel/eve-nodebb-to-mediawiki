655e22da3db92579332a5e27d61213229bf2dd66
Harry B <github@harry-b.cc>
2023-07-19 09:16:10 +0200

    feat: Read and cache all users and query categories, topics and posts

--- src/main.ts
+++ src/main.ts
@@ -20,45 +20,64 @@ async function main() {
     await client.connect();
 
     const db = client.db(DB_NAME);
-    const collection: Collection = db.collection(DB_COLLECTION_CATEGORIES);
+    const userCollection: Collection = db.collection(DB_COLLECTION_CATEGORIES);
+    const categoryCollection: Collection = db.collection(DB_COLLECTION_CATEGORIES);
+    const topicCollection: Collection = db.collection(DB_COLLECTION_CATEGORIES);
+    const postCollection: Collection = db.collection(DB_COLLECTION_CATEGORIES);
 
     console.log("Creating indexes");
     console.log("   - _key");
-    await collection.createIndex({ _key: 1 });
+    await categoryCollection.createIndex({ _key: 1 });
     console.log("   - uid");
-    await collection.createIndex({ uid: 1 });
+    await categoryCollection.createIndex({ uid: 1 });
     console.log("   - cid");
-    await collection.createIndex({ cid: 1 });
+    await categoryCollection.createIndex({ cid: 1 });
     console.log("   - tid");
-    await collection.createIndex({ tid: 1 });
+    await categoryCollection.createIndex({ tid: 1 });
     console.log("   - pid");
-    await collection.createIndex({ pid: 1 });
+    await categoryCollection.createIndex({ pid: 1 });
     console.log("   - mid");
-    await collection.createIndex({ mid: 1 });
+    await categoryCollection.createIndex({ mid: 1 });
 
-    //  .find({ _key: { $regex: /^collection:/ } })
+    // Find all users
+    const userCache = [];
+    const users = await userCollection
+      .find({ _key: { $regex: /^user:/ }, uid: { $exists: true } })
+      .toArray();
+    for (const user of users) {
+      userCache[user.uid] = user;
+    }
+
     // Find all categories
-    await collection
+    console.log("categories:");
+    const categories = await categoryCollection
       .find({ _key: { $regex: /^category:/ } })
-      .toArray()
-      .then((categories) => {
-        console.log("categories:");
-        categories.forEach(async (obj) => {
-          if (inactiveCategories.includes(obj.cid)) {
-            // Skip to the next object
-            return;
-          }
-          console.log(`${obj.cid}: ${obj.name}`);
+      .toArray();
+    for (const category of categories) {
+      if (inactiveCategories.includes(category.cid)) {
+        // Skip to the next object
+        continue;
+      }
+      console.log(`${category.cid}: ${category.name}`);
 
-          // Find all topics
-          await collection
-            .find({ cid: { $eq: obj.cid }, _key: { $regex: /^topic:/ } })
-            .toArray()
-            .then(async (topics) => {
-              console.log(topics);
-            });
-        });
-      });
+      // Find all topics
+      const topics = await topicCollection
+        .find({ cid: { $eq: category.cid }, _key: { $regex: /^topic:/ } })
+        .sort({ timestamp: 1 })
+        .toArray();
+      for (const topic of topics) {
+        console.log(`  - Topic: ${topic.title}`);
+
+        // Find all posts
+        const posts = await postCollection
+          .find({ tid: { $eq: topic.tid }, _key: { $regex: /^post:/ } })
+          .sort({ timestamp: 1 })
+          .toArray();
+        for (const post of posts) {
+          console.log(`    - Post: ${userCache[post.uid].username}`);
+        }
+      }
+    }
   } finally {
     // Ensures that the client will close when you finish/error
     await client.close();
